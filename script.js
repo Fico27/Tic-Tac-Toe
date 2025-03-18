2657e3bdfd6d5339cdf5f928247f91cfc8e604fd
Phil <182430206+Fico27@users.noreply.github.com>
2025-03-18 16:24:53 +0100

    Added in makeMove to Gameboard allowing the gameboard to be updated using row, column, player symbol

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-function Gameboard(){
+function Gameboard() {
 
     let columns = 3;
     let rows = 3;
@@ -7,15 +7,39 @@ function Gameboard(){
 
     // Here I need to create the rows
 
-    for(let i = 0; i < rows; i++){
+    for (let i = 0; i < rows; i++) {
         // Creates an empty array at that index of i (Makes a row)
         board[i] = []
-        for (let j = 0; j < columns; j++){
-            // in row i push an empy string into each column
+        for (let j = 0; j < columns; j++) {
+            // in row i push an empty string into each column
             board[i].push(" ")
         }
     }
-    console.table(board)
-    console.log(board)
+
+
+    const makeMove = (row, column, player) => {
+        board[row][column] = player
+        console.table(board)
+    }
+    return { makeMove }
+}
+
+function GameController{
+    const player1 = "Player One"
+    const player2 = "player Two"
+
+    const players = [
+        {
+            name: player1,
+            symbol: "X"
+        }
+    {
+            name: player2,
+            symbol: "O"
+        }
+    ]
+
 
 }
+
+const test = Gameboard()
